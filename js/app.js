919f8870b0372340bfe2ebade99bb9811205395c
ahmedibrahiim287 <49068254+ahmedibrahiim287@users.noreply.github.com>
2021-08-27 15:10:16 +0200

    new
    new

--- js/app.js
+++ js/app.js
@@ -1,31 +1,48 @@
+
+
+/**
+ * Define Global Variables
+ * 
+*/
+
 const navbarToggler = document.querySelector(".navbar-toggler");
-const navbarMenu = document.querySelector(".navbar ul");
-const navbarLinks = document.querySelectorAll(".navbar a");
+const sections =Array.from( document.querySelectorAll("section"));
+const menu = document.getElementById("navbar__list");
+let numberOfListItem = sections.length;
 
+
+
+// build the nav
+
+function creatItem() {
+document.addEventListener('scroll', toggelClass);
 navbarToggler.addEventListener("click", navbarTogglerClick);
-
 function navbarTogglerClick() {
   navbarToggler.classList.toggle("open-navbar-toggler");
-  navbarMenu.classList.toggle("open");
+  menu.classList.toggle("open");
 }
+  for (let section of sections) {
+     
+   let sectionName = section.getAttribute("data-nav");
+   let sectionLink = section.getAttribute('id');
+   let listItem = document.createElement('li');
+   listItem.innerHTML =`<a class='menu__link' href='#${sectionLink}'>${sectionName}</a>`
 
-
-for(let i=0; i<navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", navbarLinkClick);
-}
-
+     listItem.addEventListener('click',navbarLinkClick)
+     
 function navbarLinkClick(event) {
 
   smoothScroll(event); 
 
-  if(navbarMenu.classList.contains("open")) { 
+  if(menu.classList.contains("open")) { 
     navbarToggler.click();
   }
 
-}
-function smoothScroll(event) {
+}  
+     function smoothScroll(event) {
   event.preventDefault();
-  const targetId = event.currentTarget.getAttribute("href")==="#" ? "header" : event.currentTarget.getAttribute("href");
+  const targetId = `#${
+            event.target.innerText.replace(/\s/g, "").toLowerCase()}`
   const targetPosition = document.querySelector(targetId).offsetTop;
   const startPosition = window.pageYOffset;
   const distance = targetPosition - startPosition;
@@ -39,25 +56,41 @@ function smoothScroll(event) {
     const progress = timestamp - start;
     window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
     if (progress < duration) window.requestAnimationFrame(step);
-  }
-}
 
-// EASE
-
-function linear(t, b, c, d) {
-	return c*t/d + b;
-};
-
-function easeInOutQuad(t, b, c, d) {
-	t /= d/2;
-	if (t < 1) return c/2*t*t + b;
-	t--;
-	return -c/2 * (t*(t-2) - 1) + b;
-};
-
-function easeInOutCubic(t, b, c, d) {
+    function easeInOutCubic(t, b, c, d) {
 	t /= d/2;
 	if (t < 1) return c/2*t*t*t + b;
 	t -= 2;
 	return c/2*(t*t*t + 2) + b;
 };
+
+  }
+}
+
+    menu.appendChild(listItem);
+}
+}
+
+// Add class 'active' to section when near top of viewport
+
+function sectionInViewPort(elm) {
+  let setctionPos = elm.getBoundingClientRect();
+  return (setctionPos.top >= 0 && setctionPos.top<=400);
+}
+function toggelClass(){
+  for (let section of sections) {
+    if (sectionInViewPort(section)) {
+      if (!section.classList.contains('your-active-class')) {
+        section.classList.add('your-active-class');
+      } else {
+        section.classList.remove('your-active-class');
+      }
+    }
+  }
+}
+
+
+
+creatItem();
+document.addEventListener('scroll', toggelClass);
+
